47413d941e0cb467c3d5c5befeb5b8115f6d68ca
julia.faubus <julia.faubus@gmail.com>
2024-02-21 13:44:12 -0500

    Added comments

--- todo_list.py
+++ todo_list.py
@@ -1,18 +1,27 @@
+#function to add items
 def additems(input):
+    #add the user input as a key with a value of not done
     dict[input] = "not done"
     return dict
+
+#function to change to completed
 def changeCompleted(complete):
+    #changes the specified key have a value of "done"
     dict[complete] = "done"
     return dict
 
+#function to delete items
 def deleteItems(delete):
+    #uses the del keyword to delete that key and its value
     del dict[delete]
     return dict
 
+#asks the user what they would like to edit
 question = int(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
 ToDo = {}
 dict = {}
 
+#the if statement for adding tasks
 if question == 1:
     howMany = int(input("How many items would you like to add? "))
     x = 0
@@ -23,16 +32,19 @@ if question == 1:
         x += 1
     question = int(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
 
+#the if statement for viewing all tasks
 if question == 2:
     print(ToDo)
     question = int(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
 
+#the if statement for marking items as complete
 if question == 3:
     complete = input("What item did you complete")
     ToDo = changeCompleted(complete)  
     print(ToDo)  
     question = int(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
 
+#if statement for deleting items
 if question == 4:
     delete = input("What item would you like to delete?")
     ToDo = deleteItems(delete)
