5f95dc9b1f2cc468e669d8a34580ac7a906abe20
julia.faubus <julia.faubus@gmail.com>
2024-02-21 13:06:48 -0500

    Fixed some of the interactive portion, made changes to the additems function (so new input will no longer replace old input), and started the code to view all tasks

--- todo_list.py
+++ todo_list.py
@@ -1,16 +1,20 @@
 def additems(input):
-    dict = {}
     dict[input] = "not done"
     return dict
 
-question = str(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
-
+question = int(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
+ToDo = {}
+dict = {}
 
 if question == 1:
     howMany = int(input("How many items would you like to add? "))
     x = 0
-    while (x <= howMany):
-        input = str(input("What would you like to add to the todo list? "))
-        print(additems(input))
+    while (x < howMany):
+        adding = str(input("What would you like to add to the todo list? "))
+        ToDo = (additems(adding))
+        print(ToDo)
         x += 1
-    question = str(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
+question = str(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
+
+if question == 2:
+    print(ToDo)
