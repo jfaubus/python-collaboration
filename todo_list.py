c5feb063957b56e5f132c22aaafb0fde991c6d27
julia.faubus <julia.faubus@gmail.com>
2024-02-21 12:56:35 -0500

    Changed how the user interacts with the program

--- todo_list.py
+++ todo_list.py
@@ -3,5 +3,14 @@ def additems(input):
     dict[input] = "not done"
     return dict
 
-input = str(input("What would you like to add to the todo list? "))
-print(additems(input))
+question = str(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
+
+
+if question == 1:
+    howMany = int(input("How many items would you like to add? "))
+    x = 0
+    while (x <= howMany):
+        input = str(input("What would you like to add to the todo list? "))
+        print(additems(input))
+        x += 1
+    question = str(input("Click 1 to add new tasks, 2 to view all tasks, 3 to mark tasks as completed, and 4 to delete tasks "))
